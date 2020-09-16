f10db84b3b304b1caa28108a23278511826aee06
Wil England <willam.paul.england@gmail.com>
2020-09-16 12:51:47 -0500

    Updated launch.sh to start Python environment before pulling dependencies

--- launch.sh
+++ launch.sh
@@ -5,10 +5,10 @@ PYTHON_BINARY=${PYTHON:-python3}
 # Setup Python environment
 if [[ ! -d "env" ]]; then
     $PYTHON_BINARY -m venv env
-    pip --disable-pip-version-check install -q -r requirements.txt
 fi
 
 source env/bin/activate
+pip --disable-pip-version-check install -q -r requirements.txt
 
 # Launch the server; 'python' now points to $PYTHON_BINARY in the env
 python polymail/manage.py runserver
