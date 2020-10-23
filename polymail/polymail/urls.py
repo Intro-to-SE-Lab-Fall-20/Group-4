b597f3da2f7182d72b3a2a8f9f9841e885ddd130
Isaac Dyess <isaac.dyess@gmail.com>
2020-10-23 14:31:19 -0500

    Changed url namespace for compose so that forwarding can load already created information and new message starts empty.

--- polymail/polymail/urls.py
+++ polymail/polymail/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('logout/', views.logout),
-    path('compose/', views.compose),
+    path('compose/<str:thread_id>/', views.compose, name="compose"),
     path('emailview/<str:thread_id>/', views.emailview, name="emailview"),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'))
